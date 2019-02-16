8358153cb930254d88c95c4d526594040b4160ed
mornir <jeromepott@hotmail.com>
2019-02-16 15:08:53 +0100

    Fix links

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ fetch(API + query)
      <article class="flex items-center p-2 tracking-wide border-color-grey border-b text-black">
       <span class="flex-no-shrink font-serif text-xl code">${p.code}-15</span>
       <div>
-        <a href="https://services.vkg.ch/rest/public/georg/bs/publikation/documents/${
+        <a href="${
           p.de.link
-        }.pdf/content" target="_blank" class="block mb-1 cursor-pointer title-prescription text-black no-underline">${
+        }" target="_blank" class="block mb-1 cursor-pointer title-prescription text-black no-underline">${
         p.de.title
       }</a>
-        <a href="https://services.vkg.ch/rest/public/georg/bs/publikation/documents/${
+        <a href="${
           p.fr.link
-        }.pdf/content" target="_blank" class="block cursor-pointer title-prescription text-black text-black no-underline">${
+        }" target="_blank" class="block cursor-pointer title-prescription text-black text-black no-underline">${
         p.fr.title
       }</a>
       </div>
