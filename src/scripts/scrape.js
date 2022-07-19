2da4ab5ae4dbbc91ea9dc3a54ebbfa58737983e2
mornir <jeromepott@hotmail.com>
2022-07-19 16:49:22 +0200

    update: finish feature

--- src/scripts/scrape.js
+++ src/scripts/scrape.js
@@ -1,9 +1,26 @@
 import { parse } from 'node-html-parser'
-export function scrape(html) {
-  const root = parse(html)
-  const titlesNodes = Array.from(root.querySelectorAll('.accordion__btn'))
 
-  function getList(query) {
+export async function scrape() {
+  // TODO: check if response.ok
+
+  // TODO: retry fetch
+
+  const [responseFR, responseDE] = await Promise.all([
+    fetch('https://www.bsvonline.ch/fr/prescriptions/'),
+    fetch('https://www.bsvonline.ch/de/vorschriften/'),
+  ])
+
+  const [htmlFR, htmlDE] = await Promise.all([
+    responseFR.text(),
+    responseDE.text(),
+  ])
+
+  const rootFR = parse(htmlFR)
+  const rootDE = parse(htmlDE)
+
+  function getList(query, doc) {
+    const titlesNodes = Array.from(doc.querySelectorAll('.accordion__btn'))
+
     const links = titlesNodes
       .find((e) => e.textContent.includes(query))
       ?.nextElementSibling?.querySelectorAll('a')
@@ -12,11 +29,34 @@ export function scrape(html) {
       const [code, ...rest] = link.textContent.trim().split(' ')
       return {
         code,
-        text: rest.join(' '),
-        href: link.getAttribute('href'),
+        title: rest.join(' '),
+        link: link.getAttribute('href'),
       }
     })
   }
 
-  return getList('Directives')
+  function getBilingualList(titleFR, titleDE) {
+    const listFR = getList(titleFR, rootFR)
+    const listDE = getList(titleDE, rootDE)
+    return listFR.map((item, index) => {
+      return {
+        code: item.code,
+        fr: {
+          title: item.title,
+          link: item.link,
+        },
+        de: {
+          title: listDE[index].title,
+          link: listDE[index].link,
+        },
+      }
+    })
+  }
+
+  const norme = getBilingualList('Norme', 'Norm')
+  const directives = getBilingualList('Directives', 'Richtlinien')
+
+  return {
+    directives: [...norme, ...directives],
+  }
 }
